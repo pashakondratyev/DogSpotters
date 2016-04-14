2c336dc7f41c1069def56112c7b6c729148b8d2a
Luca Furst <rlfurst@gmail.com>
2016-04-14 00:42:09 -0400

    Almost finished login.c

--- cgi-bin/login.c
+++ cgi-bin/login.c
@@ -23,6 +23,37 @@ void unencode(char *src, char *last, char *dest)
 	*++dest = '\0';
  }
 
+/* 	scan users.txt to search for username and password	*/
+int validate_credentials(char*username, char*password){
+	FILE *fpointer;
+	fpointer = fopen(USERFILE, "r");
+	if(fpointer == NULL){
+		printf("<p>ERROR: users.txt failed to open</p>");
+	}
+	char* current_line;
+	char* file_username = fgets(current_line, MAXLEN, fpointer);
+	char* file_password = fgets(current_line, MAXLEN, fpointer);
+	char* file_fullname = fgets(current_line, MAXLEN, fpointer);
+	char* file_jobthing = fgets(current_line, MAXLEN, fpointer);
+	while(current_line != NULL){
+		if(strcmp(username, file_username) == 0 && 
+		   strcmp(password, file_password) == 0){
+			fclose(fpointer);
+			return 1;
+		}
+		else{
+			char* file_username = fgets(current_line, MAXLEN, fpointer);
+			char* file_password = fgets(current_line, MAXLEN, fpointer);
+			char* file_fullname = fgets(current_line, MAXLEN, fpointer);
+			char* file_jobthing = fgets(current_line, MAXLEN, fpointer);
+		}
+	}
+	fclose(fpointer);
+	return 0;
+
+}
+
+
 /*	unencode the username and password
 	check if username and password exist in users.txt
 	if success: load dashboard
@@ -38,11 +69,20 @@ int main(){
 	char *password_var = strstr(username, "&password=");
 	unencode(password, password + strlen(password), password);
 	unencode(username, username + strlen(username), username);
+/*	Here we have the username and password variables	*/
+	
 	printf("Content-type: text/html\n\n");
 	printf("<html>\n<body>");
-	printf("<title>Response</title>");
-	printf("<p>input: %s</p>", input);
-	printf("<p>username: %s</p>",username);
-	printf("<p>password: %s</p>",password);
-	printf("</body>\n</html>");
+/*	Check the users.txt for the username and password	*/	
+	 
+	if(validate_credentials(username, password) == 0){
+		printf("<p>Username or password is wrong/doesn't exist</p>");
+	}
+	else{
+		printf("<title>Response</title>");
+		printf("<p>input: %s</p>", input);
+		printf("<p>username: %s</p>",username);
+		printf("<p>password: %s</p>",password);
+		printf("</body>\n</html>");
+	}
 }

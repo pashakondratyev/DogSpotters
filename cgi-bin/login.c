c0f2bc72607382329727c53a2fbd4812b3db8cea
Luca Furst <rlfurst@gmail.com>
2016-04-13 22:53:58 -0400

    made login.c get the username and password vars.

--- cgi-bin/login.c
+++ cgi-bin/login.c
@@ -4,6 +4,8 @@
 #define EXTRA 10
 #define MAXINPUT MAXLEN+EXTRA
 #define USERFILE "../data/users.txt"
+#define VARLEN 8
+#include <string.h>
 
 
 void unencode(char *src, char *last, char *dest)
@@ -21,25 +23,26 @@ void unencode(char *src, char *last, char *dest)
 	*++dest = '\0';
  }
 
-
+/*	unencode the username and password
+	check if username and password exist in users.txt
+	if success: load dashboard
+	else return an error page (link back to home and login)
+*/
 int main(){
- char *lenstr;
- char input[MAXINPUT], data[MAXINPUT];
- long len;
- printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
- printf("<TITLE>Response</TITLE>\n");
- lenstr = getenv("CONTENT_LENGTH");
- if(lenstr == NULL || sscanf(lenstr,"%ld",&len)!=1 || len > MAXLEN)
- printf("<P>Error in invocation - wrong FORM probably.");
- else {
- 	FILE *f;
-	fgets(input, len+1, stdin);
-	unencode(input+EXTRA, input+len, data);
-	f = fopen(DATAFILE, "a");
-	if(f == NULL) printf("<P>Sorry, cannot store your data.");
-	else fputs(data, f);
-	fclose(f);
-	printf("<P>Thank you! Your contribution has been stored.");
- }
- return 0;
+	
+	int n = atoi(getenv("CONTENT_LENGTH"));
+	char input[MAXINPUT], data[MAXINPUT];
+	char username[MAXLEN], password[MAXLEN];
+	fgets(input, n+1, stdin);
+	sscanf(input, "username=%[^&]&password=%s", username, password);
+	char *password_var = strstr(username, "&password=");
+	unencode(password, password + strlen(password), password);
+	unencode(username, username + strlen(username), username);
+	printf("Content-type: text/html\n\n");
+	printf("<html>\n<body>");
+	printf("<title>Response</title>");
+	printf("<p>input: %s</p>", input);
+	printf("<p>username: %s</p>",username);
+	printf("<p>password: %s</p>",password);
+	printf("</body>\n</html>");
 }

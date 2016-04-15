be437f15883aaa1c11913330de06c8e9627d0d11
Luca Furst <rlfurst@gmail.com>
2016-04-15 14:29:02 -0400

    status now works. implementing status in dashboard

--- cgi-bin/dashboard.py
+++ cgi-bin/dashboard.py
@@ -8,29 +8,26 @@ import cgitb
 cgitb.enable()
 
 username = sys.argv[1]
-#print username
-print "<!Doctype html>\n<html><body>\n"
-#print "<input type="hidden" name="username" value="%s">" % username
-"""try:
-	file_name = "../dashboard.html"
-	input_file = open(file_name, "r")
-	lines=input_file.readlines()
-	i = 0
-	while i in range(0, len(lines)):
-		print lines[i]
-		i += 1
-except:
-	print "<p> Cannot open file! </p>"
-finally:
-	input_file.close()
-"""
-print """
+def make_html():
+	print "<!Doctype html>\n<html><body>\n"
+	print """
 	<title>Dashboard</title>
 	<ul>
-		<li>logout</li>
-		<li>make a friend</li>
-		<li>see a friend</li>
+		<li><a href="../index.html">logout</a></li>
+		<li>
+		<form action="makeafriend.py" method="post">
+			<input type="hidden" name="username" value = %s></input><input type="submit" value="make a friend"</input>
+		</form>
+		</li>
+		<li>
+		<form action="seeafriend.py" method="post">
+			<input type="hidden" name="username" value = %s></input><input type="submit" value="see a friend"</input>
+		</form>
+		</li>
+
 	</ul>
+	""" % (username, username )
+	print """
 	<form action="status.py" method="post">
 	<input type="hidden" name="username" value="%s">
 	<textarea name="status" cols=40 rows=6></textarea>
@@ -38,3 +35,17 @@ print """
 	<input type="submit" name="submit"></input>
 	</form>
 	""" % username
+
+make_html()
+
+#read lines from status, store them into an array
+try:
+	myfile = open("../data/status.py", "r")
+	lines = myfile.readlines()
+	i = 0
+	while i in range(0, len(lines)):
+		
+except:
+	print "cannot open file"
+finally:
+	myfile.close()
--- cgi-bin/status.py
+++ cgi-bin/status.py
@@ -10,14 +10,23 @@ cgitb.enable()
 form = cgi.FieldStorage(keep_blank_values=1)
 
 print "Content-type: text/html \r\n\r\n"
-print "<title>CGI output</title>"
-print "<p>Something</p>"
+print "<title>VALIDATE STATUS</title>"
 
 if not form["status"].value:
 	print "nothing to show"
 	#Return because it failed to validate
 else:
 	#append status to status.txt
-	print "something to show"
-	print form["username"].value
-
+#	print "<h4>something to show</h4>"
+	print "<p>User: %s</p>" % form["username"].value
+	print "<p>Status: %s</p>" % form["status"].value
+	try:
+		myfile = open("../data/status.txt", "a+")
+		myfile.write("%s %s" % (form["username"].value, form["status"].value))
+	except IOError:
+		print "<p>Error opening file</p>"
+	except:
+		print "<p>other error</p>"
+	finally:
+		myfile.close()
+	#go back to dashboard?

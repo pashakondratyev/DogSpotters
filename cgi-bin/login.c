a4535e928fde5839ea271e6367731235f1d4f3d4
pavelkondratyev <paulkondratyev@hotmail.com>
2016-04-14 20:31:14 -0400

    Cleaned up, and moved a method to parse

--- cgi-bin/login.c
+++ cgi-bin/login.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXLEN 100
-#define EXTRA 10
-#define MAXINPUT MAXLEN+EXTRA
-#define USERFILE "../data/users.txt"
-#define VARLEN 8
+#include "parse.h"
 #include <string.h>
 
 char file_username[MAXLEN];
 char file_password[MAXLEN];
 char file_fullname[MAXLEN];
 char file_jobthing[MAXLEN];
-
-void unencode(char *src, char *last, char *dest)
-{
- for(; src != last; src++, dest++)
-    if(*src == '+') *dest = ' ';
-    else if(*src == '%') {
-	int code;
-	if(sscanf(src+1, "%2x", &code) != 1) code = '?';
-		*dest = code;
-		src +=2;}
-	     
-	else *dest = *src;
-	*dest = '\n';
-	*++dest = '\0';
- }
 
 /* 	scan users.txt to search for username and password	*/
 int validate_credentials(char*username, char*password){
@@ -34,14 +15,11 @@ int validate_credentials(char*username, char*password){
 	if(fpointer == NULL){
 		printf("<p>ERROR: users.txt failed to open</p>");
 	}
-
-	int counter = 0;
 	do{
 		fgets(file_username, MAXLEN, fpointer);
 		printf("<p>%s %s</p>", username, file_username);
 		fgets(file_password, MAXLEN, fpointer);
 		printf("<p>%s %s</p>", password, file_password);
-		printf("<p>Loops: %d</p>", counter);	
 
 		if(strcmp(username, file_username) == 0 && 
 		   strcmp(password, file_password) == 0){
@@ -50,17 +28,10 @@ int validate_credentials(char*username, char*password){
 		}
 		fgets(file_fullname, MAXLEN, fpointer);
 		fgets(file_jobthing, MAXLEN, fpointer);
-	
-/*			fgets(file_username, MAXLEN, fpointer);
-			fgets(file_password, MAXLEN, fpointer);
-			fgets(file_fullname, MAXLEN, fpointer);
-			fgets(file_jobthing, MAXLEN, fpointer);
-*/		counter++;	
 	}while(feof(fpointer) == 0);
 	
 	fclose(fpointer);
 	return 0;
-
 }
 
 

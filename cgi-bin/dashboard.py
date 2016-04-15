de0f079d84bcb0d1270b17ab1645fe405d798425
Luca Furst <rlfurst@gmail.com>
2016-04-15 05:11:29 -0400

    Dashboard.py now sends username for files to read

--- cgi-bin/dashboard.py
+++ cgi-bin/dashboard.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
+"""
+THIS IS THE DASHBOARD.PY PROGRAM
+"""
 import cgi
 import sys
 import cgitb
 cgitb.enable()
 
 username = sys.argv[1]
-try:
+#print username
+print "<!Doctype html>\n<html><body>\n"
+#print "<input type="hidden" name="username" value="%s">" % username
+"""try:
 	file_name = "../dashboard.html"
 	input_file = open(file_name, "r")
 	lines=input_file.readlines()
@@ -17,4 +23,18 @@ except:
 	print "<p> Cannot open file! </p>"
 finally:
 	input_file.close()
-
+"""
+print """
+	<title>Dashboard</title>
+	<ul>
+		<li>logout</li>
+		<li>make a friend</li>
+		<li>see a friend</li>
+	</ul>
+	<form action="status.py" method="post">
+	<input type="hidden" name="username" value="%s">
+	<textarea name="status" cols=40 rows=6></textarea>
+	</br>
+	<input type="submit" name="submit"></input>
+	</form>
+	""" % username
--- cgi-bin/status.py
+++ cgi-bin/status.py
@@ -1,4 +1,7 @@
 #! /usr/bin/python
+"""
+THIS IS THE STATUS PROGRAM
+"""
 import cgitb
 import cgi
 import sys
@@ -6,16 +9,15 @@ cgitb.enable()
 
 form = cgi.FieldStorage(keep_blank_values=1)
 
-
 print "Content-type: text/html \r\n\r\n"
 print "<title>CGI output</title>"
 print "<p>Something</p>"
 
 if not form["status"].value:
 	print "nothing to show"
-	return None
 	#Return because it failed to validate
 else:
 	#append status to status.txt
 	print "something to show"
+	print form["username"].value
 

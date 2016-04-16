850534f5814017fa279f241891904d9414760e4f
pavelkondratyev <paulkondratyev@hotmail.com>
2016-04-16 00:19:43 -0400

    Fixed see friends to display displayable friends properly

--- cgi-bin/seefriends.c
+++ cgi-bin/seefriends.c
@@ -11,21 +11,21 @@ int listFriends(char* name){
 	if(fpointer == NULL){
 		printf("<p>Error: friends.txt failed to open</p>");
 	}
-	char tempString[MAXLEN];
-	char userName[MAXLEN];
+	char tempString[MAXLEN*100];
+	char *userName;
 	char *tempName;
 	printf("<form action=\"profile.cgi\" method=\"post\"><table>");
 	do{
 		fgets(tempString, MAXLEN * 100, fpointer);
-		if(strncmp(strtok(tempString," "),name, strlen(name)) == 0){
-			strcpy( tempName, strtok(tempString," "));
+		tempName = strtok(tempString," ");
+		if(strncmp(tempName,name, strlen(tempName)) == 0){
 			while(tempName != NULL){
 				printf("<tr><td><input type=\"radio\" name=\"user\" value=\"%s\"></td><td>%s</input></td></tr>",tempName, tempName);
-				tempName = strtok(tempString," ");
+				tempName = strtok(NULL," ");
 			}
 		}
 	}while(feof(fpointer)==0);
-	printf("<input type=\"hidden\" name=\"username\" value=\"%s\"></input>",tempName);
+	printf("<input type=\"hidden\" name=\"username\" value=\"%s\"></input>",name);
 	printf("<input type=\"submit\" value=\"See profile!\"></input>");
 	printf("</form>");
 	return 0;
@@ -36,7 +36,6 @@ int main(){
 	char input[MAXINPUT];
 	char username[MAXLEN];
 	fgets(input, n+1, stdin);
-//	printf("input: %s", input);
 	sscanf(input, "username=%[^&]", username);
 	unencode(username, username + strlen(username), username);
 	printf("content-type: text/html\n\n");
